25c09210e27a3a7aa3bcbb172456e3e66ca5857d
TuProfePersonal <martin.revello@gmail.com>
2025-11-04 05:53:55 -0300

    NEÓN 1000%: Tailwind + Heroicons + PostCSS → TARTA VOLANDO

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,15 +6,9 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        // COLORES DEL LOCAL
-        black:  "#000000",
-        fire:   "#E63946",
-        orange: "#F4A261",
-        neon:   "#FF00FF",
-        // FIX DEL ERROR: gray-500 ahora existe
-        gray: {
-          500: "#6B7280"
-        }
+        neon: "#00ffea",
+        "fire-red": "#ff2d00",
+        "orange-500": "#ff6b00",
       },
     },
   },
